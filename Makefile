6e015c07f21eee6527b047066ccb311e9e303fe5
tsfn88 <tsfn88@delmar.umsl.edu>
2017-11-09 10:02:34 -0600

    add some base scaffold stuff

---
+++ Makefile
@@ -0,0 +1,8 @@
+#
+# Tyler Filla
+# CS 3780
+# Project 1
+#
+
+manager: manager.cpp
+	g++ -o manager manager.cpp
---
+++ manager.cpp
@@ -0,0 +1,14 @@
+/*
+ * Tyler Filla
+ * CS 3780
+ * Project 1
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, world!\n";
+    return 0;
+}
+
